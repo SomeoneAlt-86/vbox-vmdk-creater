3be8b0384b62a33d284480c708b24b758837b845
Someone786-alt <76504556+Someone786-alt@users.noreply.github.com>
2022-01-18 07:56:54 +0530

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,10 +1,9 @@
 @ echo off
+
 cd %programfiles%\Oracle\VirtualBox
 
 set /p id="Enter Drive number: "
 
 echo Making vmdk of drive %id%
-VBoxManage internalcommands createrawvmdk -filename C:\usb.vmdk -rawdisk \\.\PhysicalDrive%id%
 
-if exist C:\usb.vmdk echo successfuly created vmdk!
-if not exist C:\usb.vmdk echo unable to write vmdk
+VBoxManage internalcommands createrawvmdk -filename C:\Drive.vmdk -rawdisk \\.\PhysicalDrive%id%
